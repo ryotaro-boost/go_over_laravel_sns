d910d14307356965c7feaa3eeb4144e13bea9bad
高倉 龍太郎 <r-takakura@semo.co.jp>
2021-08-09 18:27:04 +0900

    記事一覧画面表示にはユーザー認証が必要

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->middleware('auth');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
