a2d5ff59f7294ee768c20c292a7844e5e18e25b6
高倉 龍太郎 <r-takakura@semo.co.jp>
2021-08-09 17:30:19 +0900

    laravel/jetstreamをインストール

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', 'ArticleController@index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
